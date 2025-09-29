544e3d8a14deb9bbe2e2d1831f79a3f67886800f
mysilence1408 <mysilence1408@gmail.com>
2025-09-29 22:48:29 +0300

    backend

---
+++ backend/server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+const app = express();
+app.use(express.json());
+
+const PORT = 5000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World from Express!");
+});
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
